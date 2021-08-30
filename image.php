b3439d13891150ffece608d4ee7ce8503801c37c
Thies Mueller <github@td00.de>
2021-08-30 08:49:51 +0000

    redid the page layout now the image.php is a single file and added via iframe to the homepage.

--- image.php
+++ image.php
@@ -29,24 +29,5 @@
   }
 
 ?>
-<html>
-    <head>
-        <title>Blåhaj</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="A lot of cute Blåhaj's in the interwebz">
-        <meta name="keywords" content="Blåhaj, blahaj, hai, images">
-        <meta name="author" content="thies">
 
-    </head>
-    <body>
-    <a href="/"><button>Refresh</button></a>     <a href="https://github.com/td00/blahaj.art"><button>Contribute</button></a><br />   <a>Every refresh a random Blåhaj.</a>
-<img src="<?php echo $dir."/".$random_img ?>">
-
-<br />
-<br />
-<a href="https://thiesmueller.de">Contact me</a>
-<br />
-<a href="https://github.com/td00/blahaj.art">GitHub Project</a>
-    </body>
-</html>
+<img width="100%" height="100%" src="<?php echo $dir."/".$random_img ?>">
